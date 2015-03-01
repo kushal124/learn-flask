7d16c23a4bf482c74b26a00397ae9607c7a44d85
Kushal Khandelwal <kushal124@gmail.com>
2015-03-01 22:59:01 +0100

    Templating:Done

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -6,4 +6,18 @@ from app import app
 
 def index():
 	user = {'nickname':'Kushal'} # A fake user is being created
-	return render_template('index.html',user=user)
+	posts = [ # Fake posts
+		{
+			'author':{'nickname':'Kushal'},
+			'body': 'Learning to Use Flask'
+		},
+		{
+			'author':{'nickname':'AnotherUser'},
+			'body': 'What a way to learn Flask'
+		}
+	]
+
+	return render_template("index.html",
+							title='Home',
+							user=user,
+							posts=posts)
